e3797956013e1c4bac0108afdd715a461dc43741
krawiecpat <krawiecpat9@gmail.com>
2021-11-23 21:25:43 +0100

    zmienne

---
+++ js/main.js
@@ -0,0 +1,16 @@
+let name = 'nazwa1';
+let number = 4;
+
+name = 'nazwa2';
+number = 7;
+
+name = 'nazwa3';
+number = 66;
+
+console.log(name);
+console.log(number);
+
+name = 'nazwa4'
+let array = [4, 7, 66];
+
+console.log(array)
